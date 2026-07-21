40b33f4305e116eda497363804cdb700ec24a356
bluehimalayanpoppy <rushakundu20@gmail.com>
2026-07-21 14:14:24 -0400

    Add: jump-game - submission-0

---
+++ Data Structures & Algorithms/jump-game/submission-0.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+
+        //1,2,0,1,0
+        //pointer = 1,2,1 
+
+        int farthest = 0; 
+        //destination = current index + jump length
+        for ( int i = 0; i < nums.size(); ++i)
+        { 
+            //farthest += nums[i] + i;  
+            if ( i <= farthest) 
+            {
+                farthest = max(farthest,nums[i] + i);  
+            }
+            if (farthest >= nums.size()-1 )
+            {
+                return true;
+            }
+           
+        }
+         return false;
+    
+        
+    }
+};
