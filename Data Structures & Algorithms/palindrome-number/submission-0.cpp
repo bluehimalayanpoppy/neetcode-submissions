220e05dda20b3e2695fdec37a2b6b7b29e267ba0
bluehimalayanpoppy <rushakundu20@gmail.com>
2026-07-22 02:51:26 -0400

    Add: palindrome-number - submission-0

---
+++ Data Structures & Algorithms/palindrome-number/submission-0.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        //its a palindrome if the numbers in ascending order match the numbersin 
+        //descending order 
+
+       //convert in to string 
+       string forward = to_string(x); 
+
+
+        // 1 2 1 3 4
+        //check 1 and 4 
+        //check 2 and 3
+        // i = 0 
+        // 2, 1, 0
+        //
+        for (int i = 0; i < forward.length(); ++i)
+        {
+            if (forward[i] != forward[ forward.length() - i - 1])
+            {
+                    return false; 
+            }
+        
+        }
+        return true; 
+    }
+};
